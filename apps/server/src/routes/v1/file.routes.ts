0d6337889d2d71b61e3198cdd64b32f984ad4382
ayush-0012 <ayushbisht0012@gmail.com>
2025-10-12 00:15:35 +0530

    created a route handler for responding to user query

--- apps/server/src/routes/v1/file.routes.ts
+++ apps/server/src/routes/v1/file.routes.ts
@@ -1,9 +1,14 @@
-import { uplaodFileContent } from "@/controllers/v1/file.controllers";
+import {
+  respondToQuery,
+  uplaodFileContent,
+} from "@/controllers/v1/file.controllers";
 import type { Router } from "express";
 import express from "express";
 
 const router: Router = express.Router();
 
 router.post("/v1/file/upload", uplaodFileContent);
+
+router.post("/v1/file/query", respondToQuery);
 
 export const fileRouter: Router = router;
