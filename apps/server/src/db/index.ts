95b2de06330b6fdd55c9af7173aed2dc39bbad77
ayush-0012 <ayushbisht0012@gmail.com>
2025-10-14 13:20:00 +0530

    minor changes

--- apps/server/src/db/index.ts
+++ apps/server/src/db/index.ts
@@ -7,5 +7,9 @@ neonConfig.webSocketConstructor = ws;
 // To work in edge environments (Cloudflare Workers, Vercel Edge, etc.), enable querying over fetch
 // neonConfig.poolQueryViaFetch = true
 
+if (!process.env.DATABASE_URL) {
+  throw new Error("db url is missing");
+}
+
 const sql = neon(process.env.DATABASE_URL || "");
 export const db = drizzle(sql);
