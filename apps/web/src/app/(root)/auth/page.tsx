752e59c913823870cfbc891cef93a732bc7ac71d
ayush-0012 <ayushbisht0012@gmail.com>
2025-10-23 00:04:35 +0530

    root folder for the routes

---
+++ apps/web/src/app/(root)/auth/page.tsx
@@ -0,0 +1,7 @@
+import Auth from "@/components/root/Auth";
+
+function page() {
+  return <Auth />;
+}
+
+export default page;
---
+++ apps/web/src/app/(root)/chat/page.tsx
@@ -0,0 +1,7 @@
+import Chat from "@/components/root/Chat";
+
+function page() {
+  return <Chat />;
+}
+
+export default page;
