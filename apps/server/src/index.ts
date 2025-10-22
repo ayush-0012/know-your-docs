047c1baab92eb8da99ddb87e774c798f7286c303
ayush-0012 <ayushbisht0012@gmail.com>
2025-10-23 00:09:54 +0530

    defined a catchall route for auth handler

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,7 +1,10 @@
+import { toNodeHandler } from "better-auth/node";
+import cors from "cors";
 import "dotenv/config";
 import type { Express } from "express";
 import express from "express";
 import multer from "multer";
+import { auth } from "./lib/auth";
 import { fileRouter } from "./routes/v1/file.routes";
 
 const app: Express = express();
@@ -16,14 +19,13 @@ const corsOptions = {
 };
 
 app.use(express.json());
+app.use(cors(corsOptions));
 
-app.get("/", (req, res) => {
-  res.send("jfdkjf");
-});
+app.use("/api/auth", toNodeHandler(auth));
 
 // passing the field name for file upload in multer middleware`
 app.use("/api", upload.single("file"), fileRouter);
 
 app.listen(3000, () => {
-  console.log("server is running");
+  console.log("server is running on 3000");
 });
