9fc1d71b5238318c79470ac84c5e405703a3a227
ayush-0012 <ayushbisht0012@gmail.com>
2025-11-22 13:29:45 +0530

    modified the method to GET for sse

--- apps/server/src/routes/v1/file.routes.ts
+++ apps/server/src/routes/v1/file.routes.ts
@@ -9,6 +9,6 @@ const router: Router = express.Router();
 
 router.post("/v1/file/upload", uplaodFileContent);
 
-router.post("/v1/file/query", respondToQuery);
+router.get("/v1/file/query", respondToQuery);
 
 export const fileRouter: Router = router;
