eedc8e7ac2bc2249efbe91406d39c10c91001945
ayush-0012 <ayushbisht0012@gmail.com>
2025-11-28 13:47:09 +0530

    fix: rendering chat comp with suspense due to csr requirements

--- apps/web/src/app/(root)/chat/page.tsx
+++ apps/web/src/app/(root)/chat/page.tsx
@@ -1,9 +1,15 @@
 "use client";
+
 export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+
 import Chat from "@/components/root/Chat";
+import { Suspense } from "react";
 
-function page() {
-  return <Chat />;
+export default function Page() {
+  return (
+    <Suspense fallback={<div className="text-white p-6">Loading chat...</div>}>
+      <Chat />
+    </Suspense>
+  );
 }
-
-export default page;
