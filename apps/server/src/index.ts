f9fb075b98ba695df73cdd4366fe8f4f7df03a00
ayush-0012 <ayushbisht0012@gmail.com>
2025-12-10 21:02:34 +0530

    modified cors options

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -3,16 +3,17 @@ import cors from "cors";
 import "dotenv/config";
 import type { Express } from "express";
 import express from "express";
-import multer from "multer";
 import { auth } from "./lib/auth";
 import { chatRouter } from "./routes/v1/chat.routes";
 import { fileRouter } from "./routes/v1/file.routes";
+import { fileRouterv2 } from "./routes/v2/file.routes";
 
 const app: Express = express();
-const upload = multer({ storage: multer.memoryStorage() });
 
 const corsOptions = {
-  origin: process.env.CORS_ORIGIN || "",
+  origin: [process.env.FRONTEND_URL_DEV, process.env.FRONTEND_URL_PROD].filter(
+    (url): url is string => url !== undefined
+  ),
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
   credentials: true,
@@ -25,7 +26,8 @@ app.use(cors(corsOptions));
 app.use("/api/auth", toNodeHandler(auth));
 
 // passing the field name for file upload in multer middleware`
-app.use("/api", upload.single("file"), fileRouter);
+app.use("/api", fileRouter);
+app.use("/api", fileRouterv2);
 app.use("/api", chatRouter);
 
 app.listen(3000, () => {
